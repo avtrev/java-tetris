d3ecfc6013c185756978eff8c15d2539cf527f20
avtrev <trevorhweir@gmail.com>
2021-07-12 15:10:43 -0400

    added help menu

--- src/Grid.java
+++ src/Grid.java
@@ -2,6 +2,7 @@
 import java.awt.*;
 import javax.swing.BorderFactory;
 import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -24,7 +25,6 @@ public class Grid extends Thread implements KeyListener{
     public boolean squareTrue = false;
     public boolean tShapeTrue = false;
     
-     
     public boolean setPiece = false;
     public int lastLine = GRIDHEIGHT-1;
     public World w = new World();
@@ -50,56 +50,45 @@ public class Grid extends Thread implements KeyListener{
         
         public void shift() {
 	    	if(setPiece) {
-	        boolean[] rowFull = new boolean[GRIDHEIGHT];
-	        for(int i=0;i<rowFull.length;i++) {
-	        	rowFull[i]=true;
-	        }
-
-	  	    boolean full = true;
-	  	    boolean shiftDown = false;
-			  for(int y=0;y<GRIDHEIGHT;y++) {
-			  	for(int x=0;x<GRIDWIDTH;x++) {
-			  		//if row is full
-			  		if(w.grid[y][x] == null) {
-			  			rowFull[y] = false;
-			  		}
-			  	}
-			  }
-              //check if any rows are full
-	          for(int y=0;y<rowFull.length;y++) {
-		        	if(rowFull[y]) {
-		        		for(int x=0;x<GRIDWIDTH;x++) {
-		   	        	 	w.setAt(x,y,null);
-		        		}
-		        		//add to score after lines are cleared
-		        		score+=1000;
-		        		scoreNumber.setText(String.valueOf(score));
-		        		//speed up game after 10 lines
-		        		if(speedUpTrue && score % 10000 == 0) gameSpeed-=100;
-		        		shiftDown = true;
-		        		if(shiftDown) {
-		  	        	  //reverse for loop
-		  	        	  for(int Y=GRIDHEIGHT-1;Y>=0;Y--) {
-		  	        		  for(int X=GRIDWIDTH-1;X>=0;X--) {
-		  	        			  if(Y <= y ) {
-			  	        			  if(w.grid[Y][X] != mp &&
-			  	    					  w.grid[Y][X] != null
-			  	    					  ) {
-			  	        				  w.setAt(X, Y+1,w.getAt(X, Y));
-			  	        				  w.setAt(X, Y,null);
-			  	        				  }
-			  	        			  }
-		  			  			}
-		  	        	  }
-		  	        	  shiftDown = false;
-		  	        	  println("shiftdown");
-		  	          	}
-		        	}
-		        }
-          	  //set rowFull[] to false
-	          for(int i=0;i<rowFull.length;i++) {
-		        	rowFull[i]=false;
-		        }
+		        boolean[] rowFull = new boolean[GRIDHEIGHT];
+		        for(int i=0;i<rowFull.length;i++) rowFull[i] = true;
+				boolean full = true;
+				boolean shiftDown = false;
+				for(int y=0;y<GRIDHEIGHT;y++) {
+					for(int x=0;x<GRIDWIDTH;x++) {
+						//if row is full
+						if(w.grid[y][x] == null) rowFull[y] = false;
+					}
+				}
+				//check if any rows are full
+				for(int y=0;y<rowFull.length;y++) {
+					if(rowFull[y]) {
+						for(int x=0;x<GRIDWIDTH;x++) w.setAt(x,y,null);
+					    //add to score after lines are cleared
+						score+=1000;
+						scoreNumber.setText(String.valueOf(score));
+						//speed up game after 10 lines
+						if(speedUpTrue && score % 10000 == 0) gameSpeed-=100;
+						shiftDown = true;
+						if(shiftDown) {
+						  //reverse for loop
+						  for(int Y=GRIDHEIGHT-1;Y>=0;Y--) {
+							  for(int X=GRIDWIDTH-1;X>=0;X--) {
+								  if(Y <= y ) {
+									  if(w.grid[Y][X] != mp && w.grid[Y][X] != null) {
+										  w.setAt(X, Y+1,w.getAt(X, Y));
+										  w.setAt(X, Y,null);
+									  }
+								  }
+							  }
+						  }
+						  shiftDown = false;
+						  println("shiftdown");
+						}
+					}
+				}
+          	  	//set rowFull[] to false
+				for(int i=0;i<rowFull.length;i++) rowFull[i]=false;
           		setPiece = false;
         	}
         }
@@ -133,15 +122,45 @@ public class Grid extends Thread implements KeyListener{
     public void showScore() {
     	showScoreTrue = !showScoreTrue;
     	if(showScoreTrue) {
-    		Border blackBorder = BorderFactory.createLineBorder(Color.black);
-    		scoreNumber.setOpaque(true);
-    		headerPanel.setBorder(blackBorder);
 	        headerPanel.add(scoreLable);
 	        headerPanel.add(scoreNumber);
 	        frame.add(headerPanel,BorderLayout.NORTH);
 	        
     	} else {
     		frame.remove(headerPanel);
+    	}
+    	frame.pack();
+    }
+    
+    //SHOW HELP METHOD & VARIABLES
+    boolean showHelp = false;
+
+    JLabel menuItems[] = new JLabel[] {
+    		new JLabel("HELP MENU"),
+    		new JLabel("---------------------------------"),
+    		new JLabel("Start/Pause = Enter Or P "),
+    		new JLabel("Movement = Up Down Left Right Arrows"),
+			new JLabel("Drop Shape = Shift (Left Or Right)"),
+			new JLabel("Rotate Shape = Spacebar"),
+			new JLabel("Set Shape In Place = Z"),
+			new JLabel("Show Score = S"),
+			new JLabel("Toggle Speed Increase = F (Default On)"),
+			new JLabel("Show Help Menu= H"),
+    				};
+    JPanel helpMenuPanel = new JPanel();
+    
+    
+    public void showHelp() {
+    	showHelp = !showHelp;
+    	if(showHelp) {
+    		helpMenuPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+    		helpMenuPanel.setBackground(Color.lightGray);
+    		helpMenuPanel.setLayout(new BoxLayout(helpMenuPanel, BoxLayout.Y_AXIS));
+    		for (JLabel item : menuItems) helpMenuPanel.add(item);
+	        frame.add(helpMenuPanel,BorderLayout.EAST);
+	        
+    	} else {
+    		frame.remove(helpMenuPanel);
     	}
     	frame.pack();
     }
@@ -192,46 +211,56 @@ public class Grid extends Thread implements KeyListener{
             graphics.repaint();
         //ROTATE SELECTED SHAPE
         } else if (e.getKeyCode() == KeyEvent.VK_SPACE ) {
+        	//space bar
         	System.out.println("SPACE pressed = ROTATE SHAPE");
         		mp.rotate(w);
         	graphics.repaint();
         //SET PIECE SPAWN NEW PIECE
         } else if (e.getKeyCode() == KeyEvent.VK_Z ) {
-        	System.out.println("SHIFT pressed = SET SHAPE");
+        	//Z key
+        	System.out.println("Z pressed = SET SHAPE");
         	spawnPiece();
         	setPiece = true;
         	graphics.repaint();
     	//DROP PIECE SPAWN NEW PIECE
         } else if (e.getKeyCode() == KeyEvent.VK_SHIFT ) {
-        	System.out.println("ENTER pressed = DROP SHAPE");
+        	//shift key (left or right)
+        	System.out.println("SHIFT pressed = DROP SHAPE");
     		mp.drop(w);
         	spawnPiece();
         	setPiece = true;
         	graphics.repaint();
-        //CLEAR SCREEN
-        } else if (e.getKeyCode() == KeyEvent.VK_C) {
+        //RESET SCREEN
+        } else if (e.getKeyCode() == KeyEvent.VK_R) {
+        	//R key
         	clearGrid(w);
         	score = 0;
         	scoreNumber.setText(String.valueOf(score));
         	gameSpeed = 1000;
         	graphics.repaint();
         //TOGGLE AUTO MOVE DOWN	
-        } else if (e.getKeyCode() == KeyEvent.VK_A) {
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER || e.getKeyCode() == KeyEvent.VK_P) {
         	println("TOGGLE AUTO DOWN");
         	autoDownTrue = !autoDownTrue;
         	if(autoDownTrue) {
         		autoDown();
         	}
-    	//TOGGLE SCORE MOVEMENTS
+    	//TOGGLE SCORE
         } else if (e.getKeyCode() == KeyEvent.VK_S ) {
         	System.out.println("toggle show score");
         	showScore();
         //TOGGLE SPEED UP GAME
-        } else if (e.getKeyCode() == KeyEvent.VK_I ) {
+        } else if (e.getKeyCode() == KeyEvent.VK_F ) {
+        	//F key
         	speedUpTrue = !speedUpTrue;
         	System.out.println("toggle speed up game "+speedUpTrue);
         	if(!speedUpTrue) gameSpeed = 1000;
-        }
+        //SHOW HELP MENU
+	    } else if (e.getKeyCode() == KeyEvent.VK_H ) {
+	    	//H key
+	    	System.out.println("TOGGLE HELP MENU");
+	    	showHelp();
+	    }
     }
     
     @Override
@@ -302,7 +331,7 @@ public class Grid extends Thread implements KeyListener{
 	
 	//AUTO MOVE PIECE DOWN BOUND TO "A"
 	public boolean autoDownTrue = false;
-	public boolean speedUpTrue = false;
+	public boolean speedUpTrue = true;
 	public int gameSpeed = 1000;
 	public void autoDown() {
 		Thread autoDownThread  = new Thread() {
